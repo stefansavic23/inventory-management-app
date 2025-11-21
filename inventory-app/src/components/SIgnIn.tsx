0604445428997d853b3f9951e22750c3cc7f2591
Stefan Savić <stefansavic072@gmail.com>
2025-11-21 21:50:44 +0100

    adding user to db

--- inventory-app/src/components/SIgnIn.tsx
+++ inventory-app/src/components/SIgnIn.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react"
 import { auth } from "../config/firebase"
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 
 export const SignIn = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const signIn = async () => {
+        await createUserWithEmailAndPassword(auth, email, password)
+    }
+
     return (
         <>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
 
-            <button>Sign In</button>
+            <button onClick={signIn}>Sign In</button>
         </>
     )
 }
