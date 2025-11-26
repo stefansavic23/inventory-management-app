e4534941d4445dc30f8ad1fa2cdcbecaa5d283a7
Stefan Savić <stefansavic072@gmail.com>
2025-11-26 20:46:08 +0100

    menu component

--- inventory-app/src/App.tsx
+++ inventory-app/src/App.tsx
@@ -2,17 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LoginFinal from "./components/Login"
 import SignUp from "./components/SignUp"
 import Inventory from "./components/Inventory"
+import MenuComponent from "./components/Menu"
+
+
 
 function App() {
   return (
-    <BrowserRouter>
-
-      <Routes>
-        <Route path="/login" element={< LoginFinal />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/inventory" element={<Inventory />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <MenuComponent />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={< LoginFinal />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/inventory" element={<Inventory />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ inventory-app/src/components/Menu.tsx
@@ -0,0 +1,45 @@
+import Dropdown from '@mui/joy/Dropdown';
+import IconButton from '@mui/joy/IconButton';
+import Menu from '@mui/joy/Menu';
+import MenuButton from '@mui/joy/MenuButton';
+import MenuItem from '@mui/joy/MenuItem';
+import MoreVert from '@mui/icons-material/MoreVert';
+import Link from '@mui/joy/Link';
+
+const MenuComponent = () => {
+    return (
+        <Dropdown>
+            <MenuButton
+                slots={{ root: IconButton }}
+                slotProps={{ root: { variant: 'outlined', color: 'neutral' } }}
+            >
+                <MoreVert />
+            </MenuButton>
+            <Menu>
+                <MenuItem>
+                    <Link href="/" color="neutral">
+                        Home
+                    </Link>
+                </MenuItem>
+                <MenuItem>
+                    <Link href="/login" color="neutral">
+                        Login
+                    </Link>
+                </MenuItem>
+                <MenuItem>
+                    <Link href="/signup" color="neutral">
+                        Sign up
+                    </Link>
+                </MenuItem>
+                <MenuItem>
+                    <Link href="/inventory" color="neutral">
+                        Inventory
+                    </Link>
+                </MenuItem>
+            </Menu>
+        </Dropdown>
+    )
+
+}
+
+export default MenuComponent
