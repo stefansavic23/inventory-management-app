4ef2ad935243209faa02921f7756bf8dce8c9d87
Stefan Savić <stefansavic072@gmail.com>
2025-12-01 22:11:21 +0100

    admin panel update

--- inventory-app/src/components/AdminPanel.tsx
+++ inventory-app/src/components/AdminPanel.tsx
@@ -1,6 +1,34 @@
+import Inventory from "./Inventory"
+import Button from '@mui/joy/Button';
+import Input from '@mui/joy/Input';
+import Stack from '@mui/joy/Stack';
+import { Grid } from "@mui/material";
+
 const AdminPanel = () => {
     return (
-        <h1>admin panel</h1>
+        <>
+            <Grid container direction={"row"} sx={{
+                justifyContent: "space-between",
+                alignItems: "baseline",
+            }}>
+                <form
+                    onSubmit={(event) => {
+                        event.preventDefault();
+                        const formData = new FormData(event.currentTarget);
+                        const formJson = Object.fromEntries((formData as any).entries());
+                        alert(JSON.stringify(formJson));
+                    }}
+                >
+                    <Stack spacing={1}>
+                        <Input placeholder="Try to submit with no text!" required />
+                        <Input placeholder="It is disabled" disabled />
+                        <Button type="submit">Submit</Button>
+                    </Stack>
+                </form>
+                <hr />
+            </Grid>
+            <Inventory />
+        </>
     )
 }
 
