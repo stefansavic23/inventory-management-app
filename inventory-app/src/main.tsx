6443448bf8a7f0ab66fd0b59fa967b104f84b50e
Stefan Savić <stefansavic072@gmail.com>
2025-12-17 20:29:13 +0100

    main.tsx and App.tsx route fixes

--- inventory-app/src/main.tsx
+++ inventory-app/src/main.tsx
@@ -1,10 +1,13 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
